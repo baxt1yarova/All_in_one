228d2f3dcb204b9009dd424f6cfe72bb198642f4
baxt1yarova <baxtiyorovanasiba08@gmail.com>
2025-01-03 14:29:11 +0500

    change karta

--- kartalar/app.js
+++ kartalar/app.js
@@ -1,80 +1,56 @@
-let kartalar=[
-    'dama-chitan.jpg',
-    'dama-karyuch.jpg',
-    'dama-olma.jpg',
-    'dama-qorak.jpg',
-    'karol-chitan.jpg',
-    'karol-karyuch.jpg',
-    'karol-olma.jpg',
-    'karol-qorak.jpg',
-    'valyet-chitan.jpg',
-    'valyet-karuch.jpg',
-    'valyet-olma.jpg',
-    'valyet-qorak.jpg',
-    'dama-chitan.jpg',
-    'dama-karyuch.jpg',
-    'dama-olma.jpg',
-    'dama-qorak.jpg',
-    'karol-chitan.jpg',
-    'karol-karyuch.jpg',
-    'karol-olma.jpg',
-    'karol-qorak.jpg',
-    'valyet-chitan.jpg',
-    'valyet-karuch.jpg',
-    'valyet-olma.jpg',
-    'valyet-qorak.jpg'
-]
-let cards=document.querySelectorAll('.card')
-for(let i=0;i<=23; i++){
-    cards[i].src='img/back.jpg'
-}
-window.addEventListener('keypress',(e)=>{
-    if(e.key=='Enter'){
-        for(let i=0; i<=100;i++){
-            let rand1=Math.floor(Math.random()*100)%24
-            let rand2=Math.floor(Math.random()*100)%24
+let kartalar = [
+    'dama-chitan.jpg', 'dama-karyuch.jpg', 'dama-olma.jpg', 'dama-qorak.jpg',
+    'karol-chitan.jpg', 'karol-karyuch.jpg', 'karol-olma.jpg', 'karol-qorak.jpg',
+    'valyet-chitan.jpg', 'valyet-karuch.jpg', 'valyet-olma.jpg', 'valyet-qorak.jpg',
+    'dama-chitan.jpg', 'dama-karyuch.jpg', 'dama-olma.jpg', 'dama-qorak.jpg',
+    'karol-chitan.jpg', 'karol-karyuch.jpg', 'karol-olma.jpg', 'karol-qorak.jpg',
+    'valyet-chitan.jpg', 'valyet-karuch.jpg', 'valyet-olma.jpg', 'valyet-qorak.jpg'
+];
 
-            let t=kartalar[rand1]
-            kartalar[rand1]=kartalar[rand2]
-            kartalar[rand2]=t
-        }
-        for(let i=0;i<=23; i++){
-            cards[i].src='img/back.jpg'
-        }
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-})
-let k=0
-let firstCard=null
-let secondCard=null
-cards.forEach((item,index)=>{
-    item.onclick=()=>{
-        k++
-        item.style.transform='rotateY(180deg)'
-        setTimeout(function(){
-            item.src='img/'+kartalar[index]
-            if(k==1){
-                firstCard=item
-            }
-            else{
-                secondCard=item
-            }
-        },300)
-        
-        setTimeout(()=>{
-            if(firstCard!=null && secondCard!=null){
-                if(firstCard.src!=secondCard.src){
-                    firstCard.src='img/back.jpg'
-                    secondCard.src='img/back.jpg'
-                    k=0
-                    firstCard=null
-                    secondCard=null
+}
+
+let cards = document.querySelectorAll('.card');
+shuffle(kartalar);
+
+cards.forEach(card => {
+    card.src = 'img/back.jpg';
+});
+
+let k = 0;
+let firstCard = null;
+let secondCard = null;
+
+cards.forEach((item, index) => {
+    item.onclick = () => {
+        if (item.src.includes('back.jpg')) {
+            k++;
+            item.style.transform = 'rotateY(180deg)';
+            setTimeout(() => {
+                item.src = 'img/' + kartalar[index];
+                if (k === 1) {
+                    firstCard = item;
+                } else {
+                    secondCard = item;
+                    if (firstCard.src.split('/').pop() === secondCard.src.split('/').pop()) {
+                        firstCard = null;
+                        secondCard = null;
+                        k = 0;
+                    } else {
+                        setTimeout(() => {
+                            firstCard.src = 'img/back.jpg';
+                            secondCard.src = 'img/back.jpg';
+                            firstCard = null;
+                            secondCard = null;
+                            k = 0;
+                        }, 1000);
+                    }
                 }
-                else{
-                    k=0
-                    firstCard=null
-                    secondCard=null
-                }
-            }
-        },1200)
-    }
-})
+            }, 500);
+        }
+    };
+});
